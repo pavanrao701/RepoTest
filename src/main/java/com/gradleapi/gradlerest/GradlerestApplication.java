82406c312f22ee13348e4264ed6ccc1dd8578241
Pavan Rao A <pavanrao701@gmail.com>
2018-02-13 11:30:16 +0530

    first

---
+++ src/main/java/com/gradleapi/gradlerest/GradlerestApplication.java
@@ -0,0 +1,12 @@
+package com.gradleapi.gradlerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlerestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gradleapi/gradlerest/GradlerestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gradleapi.gradlerest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradlerestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
